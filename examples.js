1bd90897e282af1c9cd7422b9ef881376ad7a9c2
Syed Muhammad Ali Naqvi <sn07590@st.habib.edu.pk>
2023-05-01 19:11:43 +0500

    frfr

--- examples.js
+++ examples.js
@@ -23,9 +23,9 @@
     if (loaded == 0  && $(window).scrollTop() > 400) {
       loaded = 1;
   $('.second.circle').circleProgress({
-    value: 0.833
+    value: 0.944
   }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(Math.round(14657 * progress) + '<i>$ <br> raised </i>');
+    $(this).find('strong').html(Math.round(16611 * progress) + '<i>$ <br> raised </i>');
   })}});
 
 
